b07d6be7e192bc38f86a0a1db2e7d9e2c136da6c
Fadoua Charkaoui <fadoua.ch@hotmail.fr>
2020-06-03 19:36:38 +0200

    day 01 2015

--- 2015/01/01_01.cpp
+++ 2015/01/01_01.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(){
 
+    std::ifstream infile("input.txt");
+    std::string line;
 
+    infile  >> line ;
+
+    int floor = 0;
+
+    
+
+    std::cout << "floor in file is : " << floor << std::endl;
     return 0;
 }

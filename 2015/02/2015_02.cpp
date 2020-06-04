3e018aa79540259bdb773dbf3d643d242375c2de
Fadoua Charkaoui <fadoua.ch@hotmail.fr>
2020-06-04 07:03:15 +0200

    adding day 2 2015 init code

---
+++ 2015/02/2015_02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+void task1(){
+
+}
+
+void task2(){
+
+}
+
+int main(){
+
+    std::ifstream infile("input.txt");
+    std::string line;
+
+    infile  >> line ;
+
+    std::cout << line << std::endl;
+    task1();
+    task2();
+
+    return 0;
+}

76212e04595958c53c8b6c6eb26d88e851dbdfb7
Fadoua Charkaoui <fadoua.ch@hotmail.fr>
2020-06-08 11:48:22 +0200

    adding cpp for 2015 day 2

--- 2015/02/2015_02.cpp
+++ 2015/02/2015_02.cpp
@@ -2,25 +2,40 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
-void task1(){
-
-}
-
-void task2(){
-
-}
 
 int main(){
 
     std::ifstream infile("input.txt");
     std::string line;
+    int paper = 0 , ribbon = 0;
 
-    infile  >> line ;
+    while(infile >> line){
+        size_t pos = 0;
+        std::string token;
 
-    std::cout << line << std::endl;
-    task1();
-    task2();
+        std::vector<int> tmp;
 
+        while ((pos = line.find('x')) != std::string::npos) {
+            token = line.substr(0, pos);
+            tmp.push_back(std::stoi(token));
+            line.erase(0, pos + 1);
+        }
+        tmp.push_back(std::stoi(line));
+
+        std::sort(tmp.begin() , tmp.end());
+
+        paper += 2 * tmp[0] * tmp[1] + 2 * tmp[1] * tmp[2] + 2 * tmp[0] * tmp[2];
+        paper += tmp[0] * tmp[1];
+
+        ribbon += tmp[0] * tmp[1] * tmp[2];
+        ribbon +=  2 * tmp[0] + 2 * tmp[1];
+        
+
+
+    }
+
+    std::cout << paper << std::endl << ribbon << std::endl; 
     return 0;
 }

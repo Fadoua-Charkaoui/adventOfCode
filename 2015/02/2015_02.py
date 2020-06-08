2f638f0701029b1773112196d4394831220f15b5
Fadoua Charkaoui <fadoua.ch@hotmail.fr>
2020-06-08 10:56:47 +0200

    adding 2015 day 2 in python

--- 2015/02/2015_02.py
+++ 2015/02/2015_02.py
@@ -1,6 +1,24 @@
 #! /bin/env python3
 
 f = open("input.txt", "r")
-seq = f.read()
 
-print(seq)
+lines = f.readlines()
+
+total_size = 0
+total_ribbon = 0
+
+for line in lines:
+    tmp = list(map(int , line.strip().split("x")))
+
+    tmp.sort()
+    # 2*l*w + 2*w*h + 2*h*l
+    total_size += 2 * tmp[0] * tmp[1] + 2 * tmp[1] * tmp[2] + 2 * tmp[0] * tmp[2]
+
+    #total_size += tmp[0] * tmp[1] * tmp[2] / max(tmp)
+    total_size += tmp[0] * tmp[1]
+
+    total_ribbon += tmp[0] * tmp[1] * tmp[2] # bow
+    total_ribbon +=  2 * tmp[0] + 2 * tmp[1] # premiter
+
+print(total_size)
+print(total_ribbon)

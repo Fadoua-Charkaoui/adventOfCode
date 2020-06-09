eb2ec5cda9a1d8c8462cb1aa79c460a0ec88371f
Fadoua charkaoui <fadoua.ch@hotmail.fr>
2020-06-09 18:15:03 +0200

    Adding second part of 2018/01

--- 2018/01/01.py
+++ 2018/01/01.py
@@ -1,10 +1,30 @@
 f = open("input.txt", "r")
 
 lines = f.readlines()
-Freq=0
+
+#First part:
+Freq = 0
 
 for line in lines:
     Freq += int(line.strip())
     #print(line.strip())
     
 print(Freq)
+
+#Second part:
+Freq = 0
+Duplicate_found = False
+Freqs = list()
+Freqs.append(Freq)
+
+while not Duplicate_found :
+
+    for line in lines:
+        Freq += int(line.strip())
+        if Freq not in Freqs:
+            Freqs.append(Freq)
+        else:
+            print(Freq)
+            Duplicate_found = True 
+            break
+   

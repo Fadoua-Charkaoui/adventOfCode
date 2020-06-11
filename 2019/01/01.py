fb7c64e54127b853fe48f61bea60fc71b6b86a30
Fadoua charkaoui <fadoua.ch@hotmail.fr>
2020-06-11 12:42:01 +0200

    Adding python solution 2019 1st day 2nd part

--- 2019/01/01.py
+++ 2019/01/01.py
@@ -1,11 +1,29 @@
 import math
 
+# 14 2
+# 1969 966
+# 100756 50346
+
+def get_fuel_complex_required(module_mass):
+    total = 0
+    while module_mass > 8:
+        tmp = math.floor(module_mass/3-2)
+        total += tmp
+        module_mass = tmp
+
+    return total
+
+
 f = open("input.txt")
 
-mass = 0
-
+fuel_simple_req = 0
+fuel_complex_req = 0
 
 for line in f:
-    mass += math.floor(int(line.strip())/3)-2
+    fuel_simple_req += math.floor(int(line.strip())/3)-2
+    fuel_complex_req += get_fuel_complex_required(int(line.strip()))
     # print(line.strip())
-print(mass)
+
+
+print(fuel_simple_req)
+print(fuel_complex_req)

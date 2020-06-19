b80b02f863a3bc695f73e48057d75fb825c66161
Fadoua charkaoui <fadoua.ch@hotmail.fr>
2020-06-19 17:41:03 +0200

    Python solution day2 2017 part2

--- 2017/02/02.py
+++ 2017/02/02.py
@@ -1,10 +1,18 @@
 f = open("input.txt")
 sum = 0
+div_sum = 0
 
 for line in f:
     tmp = list(map(int, line.split('\t')))
     min_tmp = min(tmp)
     max_tmp = max(tmp)
     sum += max_tmp - min_tmp
+    for i in tmp:  # 5 9 2 8
+        for t in tmp:
+            if i == t:
+                continue
+            if i % t == 0:
+                div_sum += i/t
 
-print (sum)
+print(sum)
+print(div_sum)

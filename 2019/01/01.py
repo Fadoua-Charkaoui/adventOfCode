79c2dbb548f086effd200775419fb4d9f02e9e0f
Fadoua charkaoui <fadoua.ch@hotmail.fr>
2020-06-19 10:49:33 +0200

    Recursive solution for python day 1 2019 second part

--- 2019/01/01.py
+++ 2019/01/01.py
@@ -4,6 +4,16 @@ import math
 # 1969 966
 # 100756 50346
 
+
+def recusrsive(mass): 
+
+    if math.floor(mass/3) - 2 > 0:  # If fuel required > 0 
+        tmp = math.floor(mass/3) - 2 # calculate fuel required 
+        return tmp + recusrsive(tmp) # fuel required + fuel required of the previous fuel required
+    else:
+        return 0
+
+
 def get_fuel_complex_required(module_mass):
     total = 0
     while module_mass > 8:
@@ -20,8 +30,8 @@ fuel_simple_req = 0
 fuel_complex_req = 0
 
 for line in f:
-    fuel_simple_req += math.floor(int(line.strip())/3)-2
-    fuel_complex_req += get_fuel_complex_required(int(line.strip()))
+    fuel_simple_req += math.floor(int(line.strip())/3)-2   #Part one 
+    fuel_complex_req += recusrsive(int(line.strip()))      #Part two
     # print(line.strip())
 
 

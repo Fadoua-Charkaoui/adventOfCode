a1684ad4c91ce5148b425788b489651f6e061267
Fadoua charkaoui <fadoua.ch@hotmail.fr>
2020-06-19 15:37:29 +0200

    Python solution Day1 2017 second part

--- 2017/01/01.py
+++ 2017/01/01.py
@@ -1,11 +1,24 @@
 f = open("input.txt", "r")
 line = f.read()
 
-sum = 0
-    
-for index in range(len(line)) : 
-   # print(line[index])
-   if line[index] == line[index-1]:
-       sum += int(line[index])
+sum_1 = 0
+sum_2 = 0
 
-print(sum)
+for index in range(len(line)):
+   # print(line[index])
+    if line[index] == line[index-1]:
+        sum_1 += int(line[index])
+
+    if index >= int(len(line)/2):
+        new_index = index-int(len(line)/2)
+    else:
+        new_index = index+int(len(line)/2)
+
+   # new_index = (index+int(len(line)/2))%len(line)
+
+    if line[index] == line[new_index]:
+        sum_2 += int(line[index])
+
+
+print(sum_1)
+print(sum_2)
